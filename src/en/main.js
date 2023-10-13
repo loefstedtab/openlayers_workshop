4da91a2e7e05702c0a8f24cae1010966c2fe7f98
loefstedtab <theloefstedtab@gmail.com>
2023-10-13 09:30:12 -0400

    Rendering GeoJSON Complete

--- src/en/main.js
+++ src/en/main.js
@@ -1,1 +1,24 @@
-alert('Hello Workshop');
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import Link from 'ol/interaction/Link'
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countries.json',
+      }),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+map.addInteraction(new Link())
