0e662c2f3ac76674042687baf439407250d236ff
loefstedtab <theloefstedtab@gmail.com>
2023-10-13 12:54:55 -0400

    Finished Vector Data Module

--- src/en/main.js
+++ src/en/main.js
@@ -3,22 +3,94 @@ import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import Link from 'ol/interaction/Link'
+import colormap from 'colormap';
+import {DragAndDrop, Draw, Link, Modify, Snap} from 'ol/interaction';
+import {Fill, Stroke, Style} from 'ol/style';
+import {getArea} from 'ol/sphere';
+
+const min = 1e8; //The smallest Area
+const max = 2e13; //The biggest Area
+const steps = 50;
+const ramp = colormap({
+  colormap: 'blackbody',
+  nshades: steps,
+});
+
+function clamp(value, low, high) {
+  return Math.max(low, Math.min(value, high));
+}
+
+function getColor(feature) {
+  const area = getArea(feature.getGeometry());
+  const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
+  const index = Math.round(f * (steps - 1));
+  return ramp[index];
+}
 
 const map = new Map({
   target: 'map-container',
-  layers: [
-    new VectorLayer({
-      source: new VectorSource({
-        format: new GeoJSON(),
-        url: './data/countries.json',
-      }),
-    }),
-  ],
   view: new View({
     center: [0, 0],
     zoom: 2,
   }),
 });
 
-map.addInteraction(new Link())
+const source = new VectorSource();
+
+const layer = new VectorLayer({
+  source: source,
+  style: function (feature) {
+    return new Style({
+      fill: new Fill({
+        color: getColor(feature),
+      }),
+      stroke: new Stroke({
+        color: 'rgba(255,255,255,0.8)',
+      }),
+    });
+  },
+});
+
+map.addLayer(layer);
+
+map.addInteraction(new Link());
+
+map.addInteraction(
+  new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON],
+  })
+);
+
+map.addInteraction(
+  new Modify({
+    source: source,
+  })
+);
+
+map.addInteraction(
+  new Draw({
+    type: 'Polygon',
+    source: source,
+  })
+);
+
+map.addInteraction(
+  new Snap({
+    source: source,
+  })
+);
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+  source.clear();
+});
+
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const download = document.getElementById('download');
+source.on('change', function () {
+  const features = source.getFeatures();
+  const json = format.writeFeatures(features);
+  download.href =
+    'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+});
